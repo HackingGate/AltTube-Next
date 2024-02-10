8f3fe87d0c718f6be2c2db5041d2ed5175241ae5
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-10 23:54:19 +0900

    Added search results page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,3 +1,54 @@
-export default function Watch() {
-  return <h1>Hello, Results!</h1>
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+type SearchResults = {
+  items: Array<any>
+}
+
+export default function Results() {
+  const searchParams = useSearchParams()
+  const search_query = searchParams.get('search_query')
+
+  const [searchResults, setSearchResults] = useState<SearchResults>({
+    items: [],
+  })
+
+  // Fetch data when component mounts
+  useEffect(() => {
+    if (!search_query) return
+
+    const fetchData = async () => {
+      const fetchUrl = `http://localhost:8080/piped/search?q=${encodeURIComponent(search_query as string)}`
+      const response = await fetch(fetchUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      setSearchResults(await response.json())
+    }
+
+    fetchData().then(() => console.log('Fetched search results'))
+  }, [search_query])
+
+  return (
+    <div>
+      <h1>Results for {search_query}</h1>
+      {searchResults.items.length > 0 &&
+        searchResults.items.map((result, index) => (
+          <div className={'mt-4 p-4'} key={index}>
+            <Image
+              src={`http://localhost:8080${result.thumbnail}`}
+              alt={`Thumbnail for ${result.title}`}
+              width={360}
+              height={240}
+            />
+            <h2>{result.title}</h2>
+          </div>
+        ))}
+    </div>
+  )
 }
