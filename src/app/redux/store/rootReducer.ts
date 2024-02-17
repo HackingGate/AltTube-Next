04e38833af72cfcdc9cf2a2cd0bbea167f9207df
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-17 12:02:02 +0900

    Convert watch/page.tsx to redux

--- src/app/redux/store/rootReducer.ts
+++ src/app/redux/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import searchReducer from './searchSlice'
 import searchResultsSlice from '@/app/redux/store/searchResultsSlice'
+import streamResultSlice from '@/app/redux/store/streamResultSlice'
 
 const rootReducer = combineReducers({
   search: searchReducer,
   searchResults: searchResultsSlice,
+  stream: streamResultSlice,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
