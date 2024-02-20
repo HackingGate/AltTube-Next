0e63c2fd23d5ed441b793b58dd850b4eb1f5cb30
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-20 19:36:59 +0900

    Add Like/Unlike on watch page

--- src/app/redux/store/rootReducer.ts
+++ src/app/redux/store/rootReducer.ts
@@ -5,6 +5,7 @@ import streamResultSlice from '@/app/redux/slice/streamResultSlice'
 import searchSuggestionsSlice from '@/app/redux/slice/searchSuggestionsSlice'
 import userSlice from '@/app/redux/slice/user/userSlice/userSlice'
 import devicesSlice from '@/app/redux/slice/user/devicesSlice'
+import likeVideoSlice from '@/app/redux/slice/like-video/likeVideoSlice'
 
 const rootReducer = combineReducers({
   search: searchReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   user: userSlice,
   searchSuggestions: searchSuggestionsSlice,
   devices: devicesSlice,
+  likeVideo: likeVideoSlice,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
